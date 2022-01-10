97c4469b97dabbf772d989de910f2f0fc2d4823b
PowerAngelXD <692732163@qq.com>
2022-01-10 23:49:25 +0800

    [new]增加对其他运算符的支持

--- eppack/calc/calc.cpp
+++ eppack/calc/calc.cpp
@@ -3,7 +3,7 @@
 void cenv::Calculation::push(cenv::calc_unit cu){
     if (env.size() <= env_top) {
         for (int i = env.size() ; i <= env_top ; i ++)
-            env.push_back({"__NULL__", calc_unit("__NULL__", 0)});
+            env.emplace_back("__NULL__", calc_unit("__NULL__", 0));
     }
     env[env_top ++] = cu;
 }
@@ -44,6 +44,106 @@ void cenv::Calculation::run(){
                 }
             }
             else throw epperr::Epperr("TypeError", "Type uses unsupported symbol '+'", ins[i].line, ins[i].column);
+        }
+        else if(ins[i].sym == "-") {
+            auto a = pop();
+            auto b = pop();
+            if(a.first == "__INT__") {
+                if(b.first == "__INT__") {
+                    auto _a = std::any_cast<int>(a.second);
+                    auto _b = std::any_cast<int>(b.second);
+                    push(calc_unit("__INT__", _b-_a));
+                }
+                else if(b.first == "__DECI__") {
+                    auto _a = std::any_cast<int>(a.second);
+                    auto _b = std::any_cast<double>(b.second);
+                    push(calc_unit("__DECI__", _b-_a));
+                }
+            }
+            else if(a.first == "__DECI__") {
+                if(b.first == "__INT__") {
+                    auto _a = std::any_cast<double>(a.second);
+                    auto _b = std::any_cast<int>(b.second);
+                    push(calc_unit("__DECI__", _b-_a));
+                }
+                else if(b.first == "__DECI__") {
+                    auto _a = std::any_cast<double>(a.second);
+                    auto _b = std::any_cast<double>(b.second);
+                    push(calc_unit("__DECI__", _b-_a));
+                }
+            }
+            else throw epperr::Epperr("TypeError", "Type uses unsupported symbol '+'", ins[i].line, ins[i].column);
+        }
+        else if(ins[i].sym == "*") {
+            auto a = pop();
+            auto b = pop();
+            if(a.first == "__INT__") {
+                if(b.first == "__INT__") {
+                    auto _a = std::any_cast<int>(a.second);
+                    auto _b = std::any_cast<int>(b.second);
+                    push(calc_unit("__INT__", _b*_a));
+                }
+                else if(b.first == "__DECI__") {
+                    auto _a = std::any_cast<int>(a.second);
+                    auto _b = std::any_cast<double>(b.second);
+                    push(calc_unit("__DECI__", _b*_a));
+                }
+            }
+            else if(a.first == "__DECI__") {
+                if(b.first == "__INT__") {
+                    auto _a = std::any_cast<double>(a.second);
+                    auto _b = std::any_cast<int>(b.second);
+                    push(calc_unit("__DECI__", _b*_a));
+                }
+                else if(b.first == "__DECI__") {
+                    auto _a = std::any_cast<double>(a.second);
+                    auto _b = std::any_cast<double>(b.second);
+                    push(calc_unit("__DECI__", _b*_a));
+                }
+            }
+            else throw epperr::Epperr("TypeError", "Type uses unsupported symbol '*'", ins[i].line, ins[i].column);
+        }
+        else if(ins[i].sym == "/") {
+            auto a = pop();
+            auto b = pop();
+            if(a.first == "__INT__") {
+                if(b.first == "__INT__") {
+                    auto _a = std::any_cast<int>(a.second);
+                    auto _b = std::any_cast<int>(b.second);
+                    push(calc_unit("__INT__", _b/_a));
+                }
+                else if(b.first == "__DECI__") {
+                    auto _a = std::any_cast<int>(a.second);
+                    auto _b = std::any_cast<double>(b.second);
+                    push(calc_unit("__DECI__", _b/_a));
+                }
+            }
+            else if(a.first == "__DECI__") {
+                if(b.first == "__INT__") {
+                    auto _a = std::any_cast<double>(a.second);
+                    auto _b = std::any_cast<int>(b.second);
+                    push(calc_unit("__DECI__", _b/_a));
+                }
+                else if(b.first == "__DECI__") {
+                    auto _a = std::any_cast<double>(a.second);
+                    auto _b = std::any_cast<double>(b.second);
+                    push(calc_unit("__DECI__", _b/_a));
+                }
+            }
+            else throw epperr::Epperr("TypeError", "Type uses unsupported symbol '/'", ins[i].line, ins[i].column);
+        }
+        else if(ins[i].sym == "%") {
+            auto a = pop();
+            auto b = pop();
+            if(a.first == "__INT__") {
+                if(b.first == "__INT__") {
+                    auto _a = std::any_cast<int>(a.second);
+                    auto _b = std::any_cast<int>(b.second);
+                    push(calc_unit("__INT__", _b%_a));
+                }
+                else throw epperr::Epperr("TypeError", "Type uses unsupported symbol '%'", ins[i].line, ins[i].column);
+            }
+            else throw epperr::Epperr("TypeError", "Type uses unsupported symbol '%'", ins[i].line, ins[i].column);
         }
     }
     result = pop();
