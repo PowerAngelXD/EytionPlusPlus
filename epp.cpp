395550f8d56513822af345946db151ad5f650f29
PowerAngelXD <692732163@qq.com>
2022-01-14 19:56:58 +0800

    [update]更新shell

--- epp.cpp
+++ epp.cpp
@@ -12,12 +12,11 @@
 #include <sstream>
 #include <fstream>
 #include "lexer/eplex.h"
-#include "eppack/calc/calcvisitor.h"
-#include "eppack/calc/calc.h"
+#include "parser/parser.h"
 #include "eppack/error/epperr.h"
 
 int main(){
-    std::wcout<<L"Eytion++ ["<<__DATE__<<"]\nCopyright (c) PowerAngelXd\nNow version: 0.0.1"<<std::endl;
+    std::wcout<<L"Eytion++ ["<<__DATE__<<"]\nCopyright (c) PowerAngelXd\nNow version: 0.0.4"<<std::endl;
     std::string cmd;
     int code = 10;
     while(true){
@@ -28,16 +27,15 @@ int main(){
             else {
                 std::stringstream ss(cmd);
                 epplex::Lexer lexer(ss);
+                //std::cout<<ss.str()<<std::endl;
                 auto tokens = lexer.getTokenGroup();
-                for(auto token:tokens){std::cout<<token.simply_format()<<std::endl;}
+                //for(auto token:tokens){std::cout<<token.format()<<std::endl;}
                 east::astParser ast(tokens);
-                auto node = ast.gen_exprNode();
-                std::cout<<node->to_string()<<std::endl;
-                cvisitor::visitor v;
-                v.visitAddExpr(node->addexpr);
-                cenv::Calculation calc;
-                calc.ins = v.ins;
-                calc.run();
+                east::StatNode* node = ast.gen_statNode();
+                //std::cout<<node->to_string()<<std::endl;
+                parser::Parser p(*node);
+                p.parse();
+                std::cout<<std::endl;
                 code = 1;
             }
             if(code == 0)break;
