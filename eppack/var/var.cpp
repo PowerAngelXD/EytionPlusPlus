262477db676c8ac4c0844166f11665048293d910
PowerAngelXD <692732163@qq.com>
2022-01-19 16:00:22 +0800

    [update]变量系统

--- eppack/var/var.cpp
+++ eppack/var/var.cpp
@@ -1,5 +1,179 @@
-//
-// Created by liuyx2021 on 2022/1/14.
+#include "var.h"
+
+//var
+bool var::Value::isArray(){
+    return is_array;
+}
+bool var::Value::isConst(){
+    return is_const;
+}
+std::string var::Value::getType(){
+    return type;
+}
+int var::Value::val_int(){
+    if(type == "__INT__"){
+        if(!is_array) return int_val[0];
+        else throw epperr::Epperr("TypeError", "Cannot call the value of a non-array variable as an array", line, column);
+    }
+    else throw epperr::Epperr("TypeError", "The type of the variable is not Integer", line, column);
+}
+std::vector<int> var::Value::val_int_array(){
+    if(type == "__INT__"){
+        if(is_array) return int_val;
+        else throw epperr::Epperr("TypeError", "Cannot call the value of a array variable as an non-array", line, column);
+    }
+    else throw epperr::Epperr("TypeError", "The type of the variable is not Integer(array)", line, column);
+}
+float var::Value::val_deci(){
+    if(type == "__DECI__"){
+        if(!is_array) return deci_val[0];
+        else throw epperr::Epperr("TypeError", "Cannot call the value of a non-array variable as an array", line, column);
+    }
+    else throw epperr::Epperr("TypeError", "The type of the variable is not Decimal", line, column);
+}
+std::vector<float> var::Value::val_deci_array(){
+    if(type == "__DECI__"){
+        if(is_array) return deci_val;
+        else throw epperr::Epperr("TypeError", "Cannot call the value of a array variable as an non-array", line, column);
+    }
+    else throw epperr::Epperr("TypeError", "The type of the variable is not Decimal(array)", line, column);
+}
+char var::Value::val_char(){
+    if(type == "__CHAR__"){
+        if(!is_array) return ch_val[0];
+        else throw epperr::Epperr("TypeError", "Cannot call the value of a non-array variable as an array", line, column);
+    }
+    else throw epperr::Epperr("TypeError", "The type of the variable is not Char", line, column);
+}
+std::vector<char> var::Value::val_char_array(){
+    if(type == "__CHAR__"){
+        if(is_array) return ch_val;
+        else throw epperr::Epperr("TypeError", "Cannot call the value of a array variable as an non-array", line, column);
+    }
+    else throw epperr::Epperr("TypeError", "The type of the variable is not Char(array)", line, column);
+}
+std::string var::Value::val_string(){
+    if(type == "__STRING__"){
+        if(!is_array) return str_val[0];
+        else throw epperr::Epperr("TypeError", "Cannot call the value of a non-array variable as an array", line, column);
+    }
+    else throw epperr::Epperr("TypeError", "The type of the variable is not String", line, column);
+}
+std::vector<std::string> var::Value::val_string_array(){
+    if(type == "__STRING__"){
+        if(is_array) return str_val;
+        else throw epperr::Epperr("TypeError", "Cannot call the value of a array variable as an non-array", line, column);
+    }
+    else throw epperr::Epperr("TypeError", "The type of the variable is not String(array)", line, column);
+}
+bool var::Value::val_bool(){
+    if(type == "__BOOL__"){
+        if(!is_array) return bool_val[0];
+        else throw epperr::Epperr("TypeError", "Cannot call the value of a non-array variable as an array", line, column);
+    }
+    else throw epperr::Epperr("TypeError", "The type of the variable is not Bool", line, column);
+}
+std::vector<bool> var::Value::val_bool_array(){
+    if(type == "__BOOL__"){
+        if(is_array) return bool_val;
+        else throw epperr::Epperr("TypeError", "Cannot call the value of a array variable as an non-array", line, column);
+    }
+    else throw epperr::Epperr("TypeError", "The type of the variable is not Bool(array)", line, column);
+}
+void var::Value::set_val(int val){
+    if(int_val.empty())
+        int_val.push_back(val);
+    else
+        int_val[0] = val;
+    type = "__INT__";
+}
+void var::Value::set_val(std::string val){
+    str_val.push_back(val);
+    type = "__STRING__";
+}
+void var::Value::set_val(float val){
+    deci_val.push_back(val);
+    type = "__DECI__";
+}
+void var::Value::set_val(bool val){
+    bool_val.push_back(val);
+    type = "__BOOL_";
+}
+void var::Value::set_val(char val){
+    ch_val.push_back(val);
+    type = "__CHAR__";
+}
+void var::Value::set_lc(int line_, int col_){
+    line = line_;
+    column = col_;
+}
 //
 
-#include "var.h"
+//scope
+scope::Scope::Scope(std::string name_) : name(name_){}
+void scope::Scope::new_var(std::string name, var::Value val){
+    vars.push_back(std::pair<std::string, var::Value>(name, val));
+    identifier_table.push_back(name);
+}
+std::string scope::Scope::get_name(){
+    return name;
+}
+bool scope::Scope::find(std::string target){
+    for(int i = 0; i < vars.size(); i++){
+        if(vars[i].first == target) return true;
+    }
+    return false;
+}
+int scope::Scope::findI(std::string target){
+    for(int i = 0; i < vars.size(); i++){
+        if(vars[i].first == target) return i;
+    }
+    return -1;
+}
+
+scope::ScopeSet::ScopeSet(){
+    this->newScope("__epp_global_scope__");
+}
+bool scope::ScopeSet::findInAllScope(std::string name){
+    if(scope_pool[deep_count].find(name)) return true;
+    else{
+        int temp = deep_count;
+        while(true){
+            if(temp <= 0) break;
+            if(scope_pool[temp].find(name)) return true;
+            --temp;
+        }
+        return false;
+    }
+}
+int scope::ScopeSet::findInAllScopeI(std::string name){
+    if(scope_pool[deep_count].find(name)) return scope_pool[deep_count].findI(name);
+    else{
+        int temp = deep_count;
+        while(true){
+            if(temp <= 0) break;
+            if(scope_pool[temp].find(name)) return temp;
+            --temp;
+        }
+        return -1;
+    }
+};
+void scope::ScopeSet::next(){
+    deep_count += 1;
+}
+void scope::ScopeSet::back(){
+    if(deep_count == 0) deep_count = 0;
+    else deep_count -= 1;
+}
+void scope::ScopeSet::remove(){
+    if(scope_pool[scope_pool.size() - 1].get_name() == "__epp_global_scope__") return;
+    else scope_pool.pop_back();
+    back();
+}
+void scope::ScopeSet::newScope(std::string name){
+    scope_pool.push_back(Scope(name));
+}
+int scope::ScopeSet::getDeep() {
+    return deep_count;
+}
+//
--- eppack/var/var.h
+++ eppack/var/var.h
@@ -1,2 +1,89 @@
+/**
+ * @file var.h
+ * @author PowerAngelXd (692732163@qq.com)
+ * @brief variable file
+ * @date 2022-01-14
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
 #pragma once
- <iostream>
+#include <iostream>
+#include <vector>
+#include <type_traits>
+#include "../error/epperr.h"
+
+namespace var{
+    class Value{
+        std::vector<int> int_val;
+        std::vector<float> deci_val;
+        std::vector<char> ch_val;
+        std::vector<std::string> str_val;
+        std::vector<bool> bool_val;
+
+        bool is_array = false;
+        bool is_const = false;
+        std::string type = "__NULL__"; // __DECI__ __STRING__ __INT__ __CHAR__ __BOOL__
+    public:
+        int line, column;
+
+        int val_int();
+        std::vector<int> val_int_array();
+        float val_deci();
+        std::vector<float> val_deci_array();
+        char val_char();
+        std::vector<char> val_char_array();
+        std::string val_string();
+        std::vector<std::string> val_string_array();
+        bool val_bool();
+        std::vector<bool> val_bool_array();
+
+        bool isArray();
+        bool isConst();
+        std::string getType();
+        void set_val(int val);
+        void set_val(std::string val);
+        void set_val(float val);
+        void set_val(bool val);
+        void set_val(char val);
+        void set_lc(int line_, int col_);
+    };
+}
+
+namespace scope{
+    class Scope{
+        std::string name;
+    public:
+        std::vector<std::pair<std::string, var::Value>> vars;
+        std::vector<std::string> identifier_table;
+        Scope(std::string name_);
+        /**
+         * @brief create a new variable
+         * */
+        void new_var(std::string name, var::Value val);
+        /**
+         * @brief get the name of the scope
+         * */
+        std::string get_name();
+        /**
+         * @brief finds if there is a variable named Target in the Scope
+         * @return true / false
+         * */
+        bool find(std::string target);
+        int findI(std::string target);
+    };
+
+    class ScopeSet{
+        int deep_count = 0;
+    public:
+        std::vector<Scope> scope_pool;
+        ScopeSet();
+        bool findInAllScope(std::string name);
+        int findInAllScopeI(std::string name);
+        void next();
+        void back();
+        void remove();
+        void newScope(std::string name);
+        int getDeep();
+    };
+}
