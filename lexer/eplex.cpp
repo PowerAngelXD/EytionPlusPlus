2042a43943b8b60ea0f77fe73b5e0d06f4f83d80
PowerAngelXD <692732163@qq.com>
2022-01-20 21:48:40 +0800

    [update]增加关键字

--- lexer/eplex.cpp
+++ lexer/eplex.cpp
@@ -140,7 +140,7 @@ epplex::Token epplex::Lexer::Identifier() {
         ch = get();
     }
     put(ch);
-    std::vector keymap = {"out", "var", "const", "typeof"};
+    std::vector keymap = {"out", "var", "const", "typeof", "input", "delete", "area", "act", "true", "false"};
     for(auto iden : keymap)
         if (str == iden) {return {str, "__KEYWORD__", "__IDENTIFIER__", line, column};}
     return {str, "__IDENTIFIER__", "__IDENTIFIER__", line, column};
