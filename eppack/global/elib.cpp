cd7600a310d2e7e121c3a08d86aea46cb5213740
PowerAngelXD <692732163@qq.com>
2022-03-21 22:30:02 +0800

    [update]针对跨平台做的调整

--- eppack/global/elib.cpp
+++ eppack/global/elib.cpp
@@ -1,8 +1,10 @@
 #include "elib.h"
 
+#ifdef _WIN32
 void color::colprint(std::string text, color::ColorSet color, ColorSet back){
     HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
     SetConsoleTextAttribute(handle, back==ColorSet::Black?:back*16 | FOREGROUND_INTENSITY | color);
     std::cout<<text<<std::endl;
     SetConsoleTextAttribute(handle, FOREGROUND_INTENSITY | 7);
 }
+#endif
